99ed2387469ca66f0ef0dfc95fc1ad8975b853e1
rogelsond <rogelson.campos@gmail.com>
2022-03-24 23:34:28 -0300

    Criação do projeto API

---
+++ src/DevIO.Api/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DevIO.Api.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
