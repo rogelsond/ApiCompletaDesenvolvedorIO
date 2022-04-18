55126f85665d6a49e98ccc48945c3bf9e71fa3a8
rogelsond <rogelson.campos@gmail.com>
2022-04-17 22:55:18 -0300

    Criacao das ViewModels, Connection String, Injecao de Dependencia, Mapper, MainController

---
+++ src/DevIO.Api/Controllers/MainController.cs
@@ -0,0 +1,39 @@
+﻿using AutoMapper;
+using DevIO.Api.ViewModels;
+using DevIO.Business.Intefaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevIO.Api.Controllers
+{
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        // validação de notificações de erro
+        // validação de modelstate
+        // validação da operação de negócios
+
+
+    }
+
+    [Route("api/fornecedores")]
+    public class FornecedoresController : MainController
+    {
+        private readonly IMapper _mapper;
+        private readonly IFornecedorRepository _fornecedorRepository;
+
+        public FornecedoresController(IFornecedorRepository fornecedorRepository,
+                                      IMapper mapper)
+        {
+            _mapper = mapper;
+            _fornecedorRepository = fornecedorRepository;
+        }
+
+        public async Task<ActionResult<IEnumerable<FornecedorViewModel>>> ObterTodos()
+        {
+            var fornecedor = await _fornecedorRepository.ObterTodos();
+
+            return Ok(fornecedor);
+        }
+
+    }
+}
--- src/DevIO.Api/Program.cs
+++ src/DevIO.Api/Program.cs
@@ -1,9 +1,35 @@
+using DevIO.Api.Configuration;
+using DevIO.Data.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
+builder.Configuration
+    .SetBasePath(builder.Environment.ContentRootPath)
+    .AddJsonFile("appsettings.json", true, true)
+    .AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", true, true)
+    .AddEnvironmentVariables();
+
+builder.Services.AddDbContext<MeuDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//builder.Services.AddIdentityConfig(builder.Configuration);
+
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+//builder.Services.AddApiConfig();
+
+//builder.Services.AddSwaggerConfig();
+
+//builder.Services.AddLoggingConfig(builder.Configuration);
+
+builder.Services.ResolveDependencies();
+
 
 var app = builder.Build();
+//var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -13,13 +39,19 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
 
-app.UseRouting();
+//app.UseRouting();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapRazorPages();
+//app.MapRazorPages();
+
+//app.UseApiConfig(app.Environment);
+
+//app.UseSwaggerConfig(apiVersionDescriptionProvider);
+
+//app.UseLoggingConfiguration();
 
 app.Run();
